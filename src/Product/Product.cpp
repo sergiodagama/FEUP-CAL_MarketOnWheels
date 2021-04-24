deda548008bedbccd151bd8650348f00f3951c41
Eunice Amorim <up201904920@fe.up.pt>
2021-04-24 17:58:51 +0100

    Structure of Product Class

--- src/Product/Product.cpp
+++ src/Product/Product.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "Product.h"
+
+Product::Product(std::string &name, std::string cost) : name(name), price(cost) {//lança exceção de preço negativo
+    }
+
+std::string Product::getName() {
+    return std::__cxx11::string();
+}
+
+int Product::getPrice() {
+    return 0;
+}
+
+void Product::setPrice(int price) { //lança exceção se preço negativo
+
+}
--- src/Product/Product.h
+++ src/Product/Product.h
@@ -5,9 +5,17 @@
 #ifndef SRC_PRODUCT_H
 #define SRC_PRODUCT_H
 
+#include <string>
 
 class Product {
-
+private:
+    std::string name;
+    std::string price;
+public:
+    Product(std::string &name, std::string price);
+    std::string getName();
+    int getPrice();
+    void setPrice(int price);
 };
 
 
